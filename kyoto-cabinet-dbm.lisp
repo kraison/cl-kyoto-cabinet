45af8a264d33bb072a8283f1203c89ab4068ed27
Ilya Sterin <sterini@gmail.com>
2010-08-04 14:15:01 -0400

    Tested support for put/get functions

--- kyoto-cabinet-dbm.lisp
+++ kyoto-cabinet-dbm.lisp
@@ -13,15 +13,18 @@
 	   :text (apply #'format nil message message-arguments))))
 
 
-;(defmethod maybe-raise-error ((db kc-dbm) &optional message
-;			      &rest message-arguments)
-;  (let ((ecode (kcdbecode (ptr-of db))))
-;    (cond ((= +tcesuccess+ ecode)
-;           t)
-;          ((= +kcenorec+ ecode)
-;          nil)
-;          (t
-;           (apply #'raise-error db message message-arguments)))))
+(defmethod maybe-raise-error ((db kc-dbm) &optional message
+			      &rest message-arguments)
+  (let ((ecode (kcdbecode (ptr-of db))))
+    (cond ((= (foreign-bitfield-value 'dbm-return-flags '(:success))
+	      ecode)
+	   t)
+	  ((= (foreign-bitfield-value 'dbm-return-flags '(:norec))
+	      ecode)
+	   nil)
+	  (t
+	   (apply #'raise-error db message message-arguments)))))
+  
 
 (defmethod dbm-open ((db kc-dbm) filename &rest mode)
   (let ((db-ptr (ptr-of db)))
@@ -32,7 +35,6 @@
         (kcdbdel db-ptr) ; clean up on error
         (error 'dbm-error :error-code code :error-msg msg))))
   db)
-
 
 (defmethod dbm-close ((db kc-dbm))
   (kcdbclose (ptr-of db)))
@@ -52,12 +54,46 @@
 (defmethod dbm-rollback ((db kc-dbm) commit)
   (kcdbendtran (ptr-of db) NIL))
 
-(defmethod dbm-put ((db kc-dbm) key value &key mode overwrite-if-exists)
-  (let ((func (if overwrite-if-exists
-		  #'kcdbset
-		  #'kcdbadd)))
-    (funcall func (ptr-of db) key (length key) value (length value))))
 
+;; Define overloaded put methods
+
+(defmethod dbm-put ((db kc-dbm) (key string) (value string) &key (overwrite-if-exists NIL))
+  (let ((func (put-method-from overwrite-if-exists)))
+	(put-string->string db key value func)))
+
+(defmethod dbm-put ((db kc-dbm) (key string) (value vector) &key (overwrite-if-exists NIL))
+  (let ((func (put-method-from overwrite-if-exists)))
+    (put-string->octets db key value func)))
+
+(defmethod dbm-put ((db kc-dbm) (key vector) (value vector) &key (overwrite-if-exists NIL))
+  (let ((func (put-method-from overwrite-if-exists)))
+    (put-octets->octets db key value func)))
+
+(defmethod dbm-put ((db kc-dbm) (key integer) (value string) &key (overwrite-if-exists NIL))
+  (let ((func (put-method-from overwrite-if-exists)))
+    (put-int32->string db key value func)))
+
+(defmethod dbm-put ((db kc-dbm) (key integer) (value vector) &key (overwrite-if-exists NIL))
+  (let ((func (put-method-from overwrite-if-exists)))
+    (put-int32->octets db key value func)))
+
+
+;; Define overloaded get methods
 
 (defmethod dbm-get ((db kc-dbm) (key string) &optional (type :string))
-  (get-string->string db key #'kcdbget))
+  (let ((fn #'kcdbget))
+    (ecase type
+      (:string (get-string->string db key fn))
+      (:octets (get-string->octets db key fn)))))
+
+(defmethod dbm-get ((db kc-dbm) (key integer) &optional (type :string))
+  (let ((fn #'kcdbget))
+    (ecase type
+      (:string (get-int32->string db key fn))
+      (:octets (get-int32->octets db key fn)))))
+
+(defmethod dbm-get ((db kc-dbm) (key vector) &optional (type :string))
+  (let ((fn #'kcdbget))
+    (ecase type
+      (:string (get-octets->string db key fn))
+      (:octets (get-octets->octets db key fn)))))
